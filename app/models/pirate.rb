73427d48eca11e7ffe3b7f6090a7ad8fd96fbd5e
Pat Rocchio <procchio6@gmail.com>
2017-06-06 10:53:55 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize(args)
+    @name = args.fetch("name")
+    @weight = args.fetch("weight")
+    @height = args.fetch("height")
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(args)
+    @name = args.fetch("name")
+    @type = args.fetch("type")
+    @booty = args.fetch("booty")
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
